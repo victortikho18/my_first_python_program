249ae8827fdd2427129cda3e94650aebdb25e988
victortikho18 <victortikho18@gmail.com>
2025-11-02 19:54:20 +0100

    Refactor GIF creation code for clarity

--- create_gif.py
+++ create_gif.py
@@ -1,17 +1,4 @@
 
-""""
-import imageio.v3 as iio
-from PIL import Image
-
-filenames = ['photos/20251023_125234.jpg', 'photos/20251023_125540.jpg']
-images = [ ]
-
-  img = Image.open(filename)
-  img = img.rotate(90, expand=True)
-  images.append(iio.imread(filename))
-
-iio.imwrite('team.gif', images, duration = 500, loop = 0)
-"""
 
 import imageio.v3 as iio
 from PIL import Image
@@ -25,3 +12,4 @@ for filename in filenames:
     images.append(img)
 
 iio.imwrite('team.gif', images, duration=500, loop=0)
+
